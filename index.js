5d807216cd56a2c1bff528b275da8c71ad3891be
Emmy Karangwa <50500123+KarangwaEmmy@users.noreply.github.com>
2019-07-11 13:56:21 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express();
-
-const PORT = process.env.PORT || 4000
-
-app.get('/', (req, res) => res.send('Property Pro Lite End point page'))
-
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
